2228249d68e595d731c3f63d5a5b564c8a54d540
themisyaw <themisspyridhs@gmail.com>
2024-12-15 21:52:18 +0100

    15/12/2024-4

--- theme-parts/bottom-menu-content.php
+++ theme-parts/bottom-menu-content.php
@@ -4,23 +4,23 @@
 
             
     
-                <div class="align-items-end px-2 mx-2 pt-2  justify-content-between d-flex basicMenu">
+                <div class="align-items-end mx-2   justify-content-between d-flex basicMenu">
                     <div class="openBeehivesListBtn ">
-                        <div class="d-block text-center rounded">
+                        <div class="d-block text-center py-2 px-4 rounded">
                                     <?php echo get_svg_icon('beehiveBottom'); ?> 
-                                    <div class="text-dark fs-13">Beehives (<span class="beehivesTotal">20</span>)</div>
+                                    <div class="text-dark fs-13 mt-1">Beehives (<span class="beehivesTotal">20</span>)</div>
                         </div>
                     </div>
                     <div class="openBeehivesForFeedBtn ">
-                        <div class="d-block text-center  rounded">
+                        <div class="d-block text-center py-2 px-4  rounded">
                                 <?php echo get_svg_icon('beehiveForFoodBottom'); ?>
-                                <div class="text-dark fs-13">Feed (<span class="feedTotal">20</span>)</div>
+                                <div class="text-dark fs-13 mt-1">Feed (<span class="feedTotal">20</span>)</div>
                         </div>
                     </div>
                     <div class="openBeehivesForHarvestBtn ">
-                        <div class="d-block text-center  rounded">
+                        <div class="d-block text-center py-2 px-4  rounded">
                                 <?php echo get_svg_icon('beehiveForHarvestBottom'); ?>
-                                <div class="text-dark fs-13">Harvest (<span class="harvestTotal">20</span>)</div>
+                                <div class="text-dark fs-13 mt-1">Harvest (<span class="harvestTotal">20</span>)</div>
                         </div>
                     </div>
 
