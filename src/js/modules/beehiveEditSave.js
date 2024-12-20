f803075c5d04913cb48ee5edb222b1e90efafc88
themisyaw <themisspyridhs@gmail.com>
2024-12-20 23:23:43 +0100

    20/12/2024-17

--- src/js/modules/beehiveEditSave.js
+++ src/js/modules/beehiveEditSave.js
@@ -177,12 +177,12 @@ class beehiveEditSave extends beeAppListBase {
                     <h6 class="m-0 text-dark px-3 py-2 rounded bglight">${item.beehiveType === 'Κυψέλη' ? 'Hive' : 'Nucleus colony'} </h6>
                 </div>
                 <div class="col p-0  collapse" id="collapse_beehiveType" style="box-sizing: border-box;transition: height 0.35s ease;">
-                    <div class="d-flex align-items-center  p-2 pt-4 mx-2 ">
+                    <div class="d-flex align-items-center  ">
                         <input class="form-check-input " type="radio" name="beeTypeRadios" value="Κυψέλη" id="beeTypeKypseliInput" ${item.beehiveType === 'Κυψέλη' ? 'checked' : ''}>
                         <label class="form-check-label custom-radio bglight" for="beeTypeKypseliInput"></label>
                         <label class="my-0 ml-2 text-dark"  for="beeTypeKypseliInput">Hive</label>
                     </div>
-                    <div class="d-flex align-items-center  p-2 pb-3 mx-2  ">
+                    <div class="d-flex align-items-center   ">
                         <input class="form-check-input " type="radio" name="beeTypeRadios" value="Παραφυάδα" id="beeTypeParafuadaInput" ${item.beehiveType === 'Παραφυάδα' ? 'checked' : ''}>
                         <label class="form-check-label text-dark custom-radio bglight" for="beeTypeParafuadaInput"></label>
                         <label class="my-0 ml-2 text-dark " for="beeTypeParafuadaInput">Nucleus colony</label>
