d7722ffa47e40a673b0687a93d96c53a11126f8c
themisyaw <themisspyridhs@gmail.com>
2025-02-04 17:34:57 +0100

    12

--- theme-parts/beehiveEdit-content.php
+++ theme-parts/beehiveEdit-content.php
@@ -1,6 +1,6 @@
 
 <section class="my-5 container pb-5 beehiveEditSaveTab d-none">
-    <h3 class="text-start py-3"> Beehive Edit </h3>
+    <h3 class="text-start py-3"> Beehive Edit1 </h3>
     <ul class="list-group p-0 beehiveUl px-2"> 
         
     </ul>
